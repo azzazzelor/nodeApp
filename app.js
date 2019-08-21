d50efe334b291c0a2bb54d1703f3a7aaf31aca84
Vadim <azzazzelor@gmail.com>
2019-08-21 04:16:36 +0300

    db heroky changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const router = require('./routes/index');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = +process.env.PORT || 5000;
 
  
 
