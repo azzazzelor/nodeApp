8cb37dcaf58cbace179e2fca98554532b570bba8
Vadim <azzazzelor@gmail.com>
2019-09-02 14:02:59 +0300

    herocu

--- app/authentication/init.js
+++ app/authentication/init.js
@@ -1,42 +1,53 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const bcrypt = require('bcrypt');
 
 const User = require('../modules/user/userModel');
 
 const authenticationMiddleware = require('./middleware');
 
-passport.serializeUser((user, next) => {
-	next(null, user._id);
-});
-
-passport.deserializeUser((id, next) => {
-	User.findById(id, next);
-});
-
 const initPassport = () => {
-	passport.use(new LocalStrategy(
+
+	passport.serializeUser((user, next) => {
+		next(null, user._id);
+	});
+
+	passport.deserializeUser((id, next) => {
+		User.findById(id, next);
+	});
+
+	/**
+	 * Init local strategy
+	 */
+	passport.use('local', new LocalStrategy(
 		{
             usernameField: 'email',    
-            passwordField: 'password'
+            passwordField: 'password',
+			passReqToCallback : true
     	},
-    	(email, password, next) => {
+    	(req, email, password, next) => {
+			if (email)
+				email = email.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
+
     		User.findOne({email}, (err, user) => {
     			if (err) {
 	          		return next(err);
 		        }
 
 		        if (!user) {
-		        	return next(null, false, { message: 'Incorrect username or password.' });
+		        	return next(null, false, req.flash('loginMessage', 'Incorrect username or password.'));
 		        }
 
 		        bcrypt.compare(password, user.password, (err, isValid) => {
+
 					if (err) {
 						return next(err);
 					}
 
 					if (!isValid) {
-						return next(null, false, { message: 'Incorrect username or password.' });
+						return next(null, false, req.flash('loginMessage', 'Incorrect username or password.'));
 					}
 
 					return next(null, user);
@@ -46,7 +57,102 @@ const initPassport = () => {
     	}
 	));
 
-	passport.authenticationMiddleware = authenticationMiddleware;
-};
+	/**
+	 * Init facebook strategy
+	 */
+	passport.use('facebook', new FacebookStrategy(
+		{
+			clientID: process.env.FACEBOOK_APP_ID,
+			clientSecret: process.env.FACEBOOK_SECRET,
+			callbackURL: process.env.FACEBOOK_CALLBACK,
+			passReqToCallback : true
+		},
+		(req, accessToken, refreshToken, profile, next) => {
+			process.nextTick(() => {
+				console.log(profile.id);
+				User.findOne({ 'facebook.id' : profile.id }, (err, user) => {
+					if (err) {
+						return next(err);
+					}
+
+					if (!user) {
+						return next(null, false, req.flash('facebookError', 'User are not exist.'));
+					}
+
+					// if there is a user id already but no token (user was linked at one point and then removed)
+					if (!user.facebook.token) {
+						user.facebook.token = accessToken;
+						user.facebook.firstName  = profile.name.givenName;
+						user.facebook.lastName = profile.name.familyName;
+						//user.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+						user.save(function(err) {
+							if (err)
+								return next(err);
+
+							return next(null, user);
+						});
+					}
+
+					return next(null, user); // user found, return that user
+
+				});
+			});
+		}
+	));
+
+
+   /**
+	 * Init google strategy
+	 */
+    passport.use(new GoogleStrategy({
+
+        clientID        : process.env.GOOGLE_APP_ID,
+        clientSecret    : process.env.GOOGLE_SECRET,
+        callbackURL     : process.env.GOOGLE_CALLBACK,
+        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+
+    },
+    function(req, token, refreshToken, profile, next) {
+
+			process.nextTick(() => {
+				// console.log(profile.id);
+				User.findOne({ 'google.id' : profile.id }, (err, user) => {
+
+					if (err) {
+						return next(err);
+					}
+
+					if (!user) {
+						return next(null, false, req.flash('gooogleError', 'User are not exist.'));
+					}
+
+					// if there is a user id already but no token (user was linked at one point and then removed)
+					if (!user.google.token) {
+						user.google.token = token;
+						user.google.name  = profile.displayName;
+						// user.google.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
+
+						user.save(function(err) {
+							if (err)
+								return next(err);
+
+							return next(null, user);
+						});
+					}
+
+					return next(null, user); // user found, return that user
+
+				
+				});
+			});
+		}
+	))
+
+		passport.authenticationMiddleware = authenticationMiddleware;
+}
+
+	
+
 
 module.exports = initPassport;
